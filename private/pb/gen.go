512e4b7dbcc055e56810c9bea67a98a96ca241eb
mbesida <maks.besida@gmail.com>
2024-12-17 14:50:43 +0200

    removed unnecessary files

--- private/pb/gen.go
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2024 Akave
-// See LICENSE for copying information.
-
-// Package pb used to contain proto files and generated go files
-package pb
-
-//go:generate sh -c "protoc --proto_path=. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative $(ls *.proto)"
